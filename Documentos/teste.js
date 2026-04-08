a5843f07de34adb5a0b3c75ad14a0f97a2a477ed
caio-ohya <caio.e.lima@escola.pr.gov.br>
2026-04-08 11:52:37 -0300

    Add files via upload

---
+++ Documentos/teste.js
@@ -0,0 +1,27 @@
+//Aluno Caio Ohya 3 tec
+
+
+//1. Exiba a mensagem "Olá Mundo"
+console.log("Olá Mundo");
+
+//2. Crie uma variavel com seu nome e exiba o valor
+let nome = "Caio Ohya";
+console.log(nome); 
+
+//3. Some dois números e exiba o resultado
+let soma = 2 + 2;
+console.log(soma);
+
+//4.Subtraia dois numeros e exia o valor
+let subtraia = 4 - 2;
+console.log(subtraia);
+
+//5. Descura o resto da divisão de um número por outro 
+let resto = 10 % 3;
+console.log(resto);
+
+//6. Crie uma váriavel booleana(True False) e exiba seu valor
+let estaGanhando = true;
+console.log(estaGanhando);
+
+//7. Verifique se um número pe maior que outro e exiba o resultado
